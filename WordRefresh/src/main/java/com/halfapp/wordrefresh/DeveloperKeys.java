ff6ab51c98f4bdbf9d717b2b49baa1775b498e4e
Miha Susnik <mihec.susnik@gmail.com>
2013-12-26 23:03:37 +0100

    Commit everything <3

---
+++ WordRefresh/src/main/java/com/halfapp/wordrefresh/DeveloperKeys.java
@@ -0,0 +1,10 @@
+package com.halfapp.wordrefresh;
+
+/**
+ * This is used for hiding Google Analytics and Wordnik API passwords and tracking IDs.
+ */
+public class DeveloperKeys
+{
+    public static String WORDNIK_API_KEY = "XX";
+    public static String GOOGLE_ANALYTICS_KEY = "XX";
+}
